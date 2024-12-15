49534888044336ad2e29dc08b1b466dd672f348b
Mama africa <k_vashalomidze@mail.ru>
2024-12-15 15:24:18 +0400

    modified sslconfig class

--- src/main/java/org/devexperts/config/SSLConfig.java
+++ src/main/java/org/devexperts/config/SSLConfig.java
@@ -2,12 +2,16 @@ package org.devexperts.config;
 
 import org.apache.catalina.connector.Connector;
 import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SSLConfig {
+    @Value("${server.port}")
+    private int serverPort;
+
     @Bean
     public TomcatServletWebServerFactory servletContainer() {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
@@ -17,7 +21,7 @@ public class SSLConfig {
 
     private Connector createSslConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setPort(10004);
+        connector.setPort(serverPort);
         connector.setSecure(true);
         connector.setScheme("https");
         Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
